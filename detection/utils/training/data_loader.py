4a542d260521d1facf8a577e4b98300aaf833018
Constantin Pape <constantin.pape@informatik.uni-goettingen.de>
2026-01-09 10:13:21 +0100

    Add csv dataset and other minor changes

--- detection/utils/training/data_loader.py
+++ detection/utils/training/data_loader.py
@@ -20,7 +20,7 @@ def _load_dataset(
     raw_paths,
     label_paths,
     raw_transform, transform,
-    patch_shape, 
+    patch_shape,
     raw_key=None,
     eps=0.00001, sigma=None,
     lower_bound=None, upper_bound=None,
@@ -70,28 +70,36 @@ def create_data_loader(
         raw_paths=train_images, raw_key=raw_key, label_paths=train_labels, patch_shape=patch_shape,
         raw_transform=raw_transform, transform=transform, eps=eps, sigma=sigma,
         lower_bound=lower_bound, upper_bound=upper_bound, n_samples=n_samples_train,
+        dataset_class=dataset_class,
     )
     val_set = _load_dataset(
         raw_paths=val_images, raw_key=raw_key, label_paths=val_labels, patch_shape=patch_shape,
         raw_transform=raw_transform, transform=transform, eps=eps, sigma=sigma,
         lower_bound=lower_bound, upper_bound=upper_bound, n_samples=n_samples_val,
+        dataset_class=dataset_class,
     )
-    test_set = _load_dataset(
-        raw_paths=test_images, raw_key=raw_key, label_paths=test_labels, patch_shape=patch_shape,
-        raw_transform=raw_transform, transform=transform, eps=eps, sigma=sigma,
-        lower_bound=lower_bound, upper_bound=upper_bound,
-    )
+    if test_images is not None:
+        test_set = _load_dataset(
+            raw_paths=test_images, raw_key=raw_key, label_paths=test_labels, patch_shape=patch_shape,
+            raw_transform=raw_transform, transform=transform, eps=eps, sigma=sigma,
+            lower_bound=lower_bound, upper_bound=upper_bound,
+            dataset_class=dataset_class,
+        )
+    else:
+        test_set = None
 
     # put into DataLoader
     train_dataloader = DataLoader(train_set, batch_size=batch_size, shuffle=True,
                                   num_workers=num_workers)
     val_dataloader = DataLoader(val_set, batch_size=batch_size, shuffle=True,
                                 num_workers=num_workers)
-    test_dataloader = DataLoader(test_set, batch_size=batch_size, shuffle=True,
-                                 num_workers=num_workers)
+    if test_set is None:
+        test_dataloader = None
+    else:
+        test_dataloader = DataLoader(test_set, batch_size=batch_size, shuffle=True,
+                                     num_workers=num_workers)
+        test_dataloader.shuffle = True
 
     train_dataloader.shuffle = True
     val_dataloader.shuffle = True
-    test_dataloader.shuffle = True
-
     return train_dataloader, val_dataloader, test_dataloader
